3eb6ea26ec6f0c625117afb48d2cb4af4bbb3c19
Alexander Thomas <doctor.lex@gmail.com>
2017-01-21 14:49:41 +0100

    Add version 0.96 to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # SRTLab
-SubRip subtitle file converter
+*SubRip subtitle file converter*
+by Alexander Thomas, aka Dr. Lex
+Contact: visit https://www.dr-lex.be/lexmail.html
+&nbsp;&nbsp;&nbsp;&nbsp;or use my gmail address "doctor.lex".
+
+
+## What is it?
+This is a Perl script that can perform certain operations on SubRip (.srt) subtitle files. For instance, it can scale and offset the time stamps of all subtitles based on two pairs of current and expected time values. It can also check files for subtitles that appear too briefly or overly long, and attempt to fix subs that appear too briefly (which is of course not always possible), as well as remove overlap between subtitles.
+
+It is a bit of a work in progress and I plan to add some more features, but I released it to the public since it is already useful in its current form.
+
+## Installing
+Put it anywhere in a place that is in your executable PATH. Or always run it by specifying its full path.
+
+## Usage
+To get extended help, run the script with the switch '-h'.
+
+The normal mode of operation is to run the script as:
+`srtlab.pl input.srt > output.srt`
+Which uses the standard redirect mechanism to write the output to a file. When running the script without the '>' part, it will simply print output on the console.
+
+BEWARE: do not try this:
+`srtlab.pl input.srt > input.srt`
+This will destroy input.srt and leave you with an empty file. If you want to overwrite the input directly, instead run the script as follows and be aware that you will not be able to fix any mistakes you made unless you have a back-up of the file:
+`srtlab -e input.srt`
+
+The -L option can only fix too short subtitles if there is enough empty time after them. Otherwise more manual work will be required to fix the poorly made subtitle file. This option does not shorten 'sticky' subtitles (i.e. that appear too long) because these can sometimes be intentional. You should check the reported sticky subs yourself and fix them if necessary.
+In case of overlapping subtitles, -L will cut off the first subtitle in an overlapping pair at the time where the second one starts.
+
+The -H switch will cause the script to remove the most common non-verbal annotations in subtitles for the hearing impaired (like [CLEARS THROAT] and character names). This can be useful if you want to play your film silently without missing out on any of the dialogue, or if you want to prepare a subtitle file for translation. In most cases you should combine -H with -c.
+If you provide the -H switch twice, it will try a wider range of patterns to strip non-dialogue subtitles. You should only use this if a single -H does not work satisfactorily, because -HH has a higher risk of removing parts of regular subtitles.
+
+At this time the script will only work with UTF-8, UTF-16 or Windows Latin 1 encoded files. It will only detect UTF-x encodings if the file starts with a Unicode BOM character, otherwise it will default to Latin 1 (cp1252). If you usually work with subtitles in another encoding, you can adjust the last lines of the script. My goal is to make the script smarter in future versions such that it auto-detects all common encodings. My advice is to convert all your srt files to Unicode unless your media player does not support it. The 8-bit encodings are a thing of the past.
+
+## License
+This program is released under the GNU General Public License. See the source file and COPYING for more details.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
---
+++ srtlab.pl
@@ -0,0 +1,526 @@
+#!/usr/bin/perl
+# SRTLab by Alexander Thomas
+# A tool to modify .srt files.
+#
+# Version 0.9  (2009/08): unfinished
+# Version 0.91 (2010/03): added CRLF option
+# Version 0.92 (2010/04): split parser & renderer, added time check & fix
+# Version 0.93 (2011/01): more robustness against crappy files
+# Version 0.94 (2011/08): improved hearing-aid filtering
+# Version 0.95 (2011/09): added extra hearing-aid filtering mode
+# Version 0.96 (2012/07): overlap detection and removal
+#
+# Copyright (C) 2012  Alexander Thomas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+
+my $VERSION = 0.96;
+
+# TODO: improve handling of encodings and allow to override input encoding detection.
+#
+# Ideas for new features:
+# Extension of auto-correction: if of two single-line consecutive subs, one
+#  is shown too long and one too short, and they're within a reasonable time,
+#  they can be merged. But this will require manual intervention to include
+#  the mandatory "- " indicator when fusing dialogue from different actors.
+# Similarly, too long subs could be split up (although those mostly stem from
+#  hopelessly poor translation anyway)
+#
+# Automatic syncing of a subtitle file given another file with correct sync
+#  (e.g. in another language or a worthless translation with good sync):
+# This boils down to a least squares estimation of S and O but with possibly
+#  missing and/or superfluous data points. Subtitles may be split up in the
+#  other language, some may be omitted, or there may be extra subs that aren't
+#  in the target file. Most weight should be given to subs after a period of
+#  silence, because those are the most reliable points for syncing.
+# In other words, this is a whole lot of work so it's unlikely that I'll ever
+#  implement it.
+# If it works however, it's not a big step up to actually detect speech in the
+#  film soundtrack and use that data as the reference. Full auto-sync, yeah!
+
+
+#===== Defaults =====#
+# Minimum ratio of seconds/#characters in a subtitle for length check.
+# This number is tuned for Dutch, it may be different for other languages.
+my $minRatio = .034;
+# Maximum ratio of seconds/#characters, above which a subtitle will be
+# marked as 'sticky' if it appears longer than 3 seconds.
+my $stickRatio = .2;
+# Absolute minimum duration in seconds for any subtitle, no matter how short
+my $minDur = .8;
+# Gap to leave between end of this subtitle and start of next when auto-correcting
+my $gap = .08;
+
+my $scale = 1.0;
+my $offset = 0.0;
+my ($encodingIn,$encodingOut);
+my ($bAuto,$bVerbose,$bClean,$bCRLF,$bHasBOM,$tSaveBOM) = (0,0,0,0,0,-1);
+my ($bCheckLength,$bFixLength,$bInPlace,$bTextOnly,$bNukeHA,$bNukeHarder) = (0,0,0,0,0,0);
+my @insertInd = ();
+my @inserTime = ();
+
+
+sub printUsage
+{ #      12345678901234567890123456789012345678901234567890123456789012345678901234567890
+	print "srtlab [options] file1.srt [file2.srt ...] > output.srt\n"
+	     ."SRT file editing tool.\n"
+	     ."  Multiple input files will be joined. You should make sure in such case that\n"
+	     ."    the time stamps do not overlap and the character encodings are the same.\n"
+	     ."  Options:\n"
+	     ."  Time values must be in the format [-]HH:MM:SS.sss, or a floating-point number\n"
+	     ."    representing seconds.\n"
+	     ."  -s S: scale all timestamps.\n"
+	     ."    S can be a floating-point number or any of these shortcuts:\n"
+	     ."    NTSCPAL:  0.95904    = 23.976/25 (subs for NTSC framerate to PAL video)\n"
+	     ."    PALNTSC:  1.04270938 = 25/23.976 (PAL framerate to NTSC)\n"
+	     ."    NTSCFILM: 0.999      = 23.976/24 (NTSC framerate to film)\n"
+	     ."    PALFILM:  1.04166667 = 25/24     (PAL framerate to film)\n"
+	     ."    FILMNTSC: 1.001001   = 24/23.976 (film framerate to NTSC)\n"
+	     ."    FILMPAL:  0.96       = 24/25     (film framerate to PAL)\n"
+	     ."  -o O: offset all timestamps by time O.  Offset is added after scaling, i.e.\n"
+	     ."    new times are calculated as S*t+O.\n"
+	     ."  -a Ta1 Ta2 Tb1 Tb2: automatically calculate S and O.\n"
+	     ."    Ta1 is the time at which a subtitle appears in the current SRT file, Ta2 is\n"
+	     ."    where it should appear in the output. The same for Tb1 and Tb2, for another\n"
+	     ."    subtitle. For maximum accuracy, use the earliest possible subtitle for Ta\n"
+	     ."    and the last subtitle for Tb.\n"
+	     ."  -b Ta1 Ta2: like -a, but only calculate the offset O.\n"
+	     ."  -i I: insert a new subtitle at index I (in the original file). This command\n"
+	     ."    can be repeated, e.g. to insert two subs at index 3, use -ii 3 3.\n"
+	     ."  -j J: insert a new subtitle at original time J (can be repeated as well).\n"
+	     ."  -l: report subtitles that appear too briefly or overly long, or overlap.\n"
+	     ."  -L: report and attempt to repair subtitles that appear too briefly or overlap.\n"
+	     ."  -c: remove empty subtitles (empty = really empty, no whitespace characters).\n"
+	     ."  -r: maintain Redmond-style compatibility with typewriters (CRLF). If this\n"
+	     ."    option is not enabled, any existing CR will be obliterated.\n"
+	     ."  -u: save output in UTF-8.\n"
+	     ."  -m: add BOM character to output file if it is Unicode.\n"
+	     ."  -M: do not add BOM character to output file (default is same as input).\n"
+	     ."  -e: in-place editing: overwrite first file instead of printing to stdout\n"
+	     ."    (BE CAREFUL!)\n"
+	     ."  -t: strip all SRT formatting and only output the text.\n"
+	     ."  -H: attempt to remove typical non-verbal annotations in subs for the hearing\n"
+       ."    impaired, e.g. (CLEARS THROAT).  You may want to combine this with -c.\n"
+       ."    Repeat -H to try to remove non-capitalized annotations.\n"
+	     ."  -v: verbose mode.\n"
+	     ."  -V: print version and exit.\n";
+}
+
+
+my @files = ();
+
+if( $#ARGV < 0 ) {
+	printUsage();
+	exit(1);
+}
+
+# Parse command line arguments
+while( $#ARGV >= 0 ) {
+	my $arg = shift;
+	if( substr( $arg, 0, 1 ) eq '-' ) {
+		my @switches = split(//,$arg);
+		shift(@switches);
+		foreach my $sw (@switches) {
+			if( $sw eq 's' ) {
+				$scale = shift;
+				if( ! defined($scale) || ! isScale($scale) ) {
+					print STDERR "Scale must be a positive floating-point number or a supported symbol\n";
+					exit(2);
+				}
+				$scale = fromScale($scale);
+			}
+			elsif( $sw eq 'o' ) {
+				$offset = shift;
+				if( ! defined($offset) || ! (isFloat($offset) || isHMS($offset)) ) {
+					print STDERR "Offset must be a floating-point number or [-]HH:MM:SS.sss\n";
+					exit(2);
+				}
+				if( isHMS($offset) ) {
+					$offset = fromHMS($offset);
+				}
+			}
+			elsif( $sw eq 'a' ) {
+				my ($Ta1,$Ta2,$Tb1,$Tb2) = (shift, shift, shift, shift);
+				if( ! defined($Ta1) || ! (isFloat($Ta1) || isHMS($Ta1)) ||
+				    ! defined($Ta2) || ! (isFloat($Ta2) || isHMS($Ta2)) ||
+				    ! defined($Tb1) || ! (isFloat($Tb1) || isHMS($Tb1)) ||
+				    ! defined($Tb2) || ! (isFloat($Tb2) || isHMS($Tb2)) ) {
+					print STDERR "-a expects four arguments, which must be floating-point numbers or [-]HH:MM:SS.sss\n";
+					exit(2);
+				}
+				($Ta1,$Ta2,$Tb1,$Tb2) = (fromHMS($Ta1), fromHMS($Ta2), fromHMS($Tb1), fromHMS($Tb2));
+				$scale = ($Ta2-$Tb2)/($Ta1-$Tb1);
+				$offset = ($Tb2*$Ta1-$Ta2*$Tb1)/($Ta1-$Tb1);
+				$bAuto = 1;
+			}
+			elsif( $sw eq 'b' ) {
+				my ($Ta1,$Ta2) = (shift, shift);
+				if( ! defined($Ta1) || ! (isFloat($Ta1) || isHMS($Ta1)) ||
+				    ! defined($Ta2) || ! (isFloat($Ta2) || isHMS($Ta2)) ) {
+					print STDERR "-b expects two arguments, which must be floating-point numbers or [-]HH:MM:SS.sss\n";
+					exit(2);
+				}
+				$offset = fromHMS($Ta2)-fromHMS($Ta1);
+				$bAuto = 1;
+			}
+			elsif( $sw eq 'i' ) {
+				my $ind = shift;
+				if( ! defined($ind) || $ind < 1 ) {
+					print STDERR "-i expects an integer greater than 0 as next argument.\n";
+					exit(2);
+				}
+				push(@insertInd,$ind);
+			}
+			elsif( $sw eq 'j' ) {
+				my $tim = shift;
+				if( ! defined($tim) || ! (isFloat($tim) || isHMS($tim)) ) {
+					print STDERR "-j expects a floating-point number or HH:MM:SS.sss time as next argument.\n";
+					exit(2);
+				}
+				push(@inserTime,fromHMS($tim));
+			}
+			elsif( $sw eq 'l' ) { $bCheckLength = 1; }
+			elsif( $sw eq 'L' ) {
+				$bCheckLength = 1;
+				$bFixLength = 1;
+			}
+			elsif( $sw eq 'c' ) { $bClean = 1; }
+			elsif( $sw eq 'r' ) { $bCRLF = 1; }
+			elsif( $sw eq 'u' ) { $encodingOut = 'UTF-8'; }
+			elsif( $sw eq 'm' ) { $tSaveBOM = 1; }
+			elsif( $sw eq 'M' ) { $tSaveBOM = 0; }
+			elsif( $sw eq 'e' ) { $bInPlace = 1; }
+			elsif( $sw eq 't' ) { $bTextOnly = 1; }
+			elsif( $sw eq 'H' ) {
+				if($bNukeHA) {
+					$bNukeHarder = 1;
+				} else {
+					$bNukeHA = 1;
+				}
+			}
+			elsif( $sw eq 'v' ) { $bVerbose = 1; }
+			elsif( $sw eq 'V' ) {
+				print "SRTLab $VERSION by Alexander Thomas\n";
+				exit(0);
+			}
+			elsif( $sw eq 'h' ) {
+				printUsage();
+				exit(0);
+			}
+			else {
+				print STDERR "Ignoring unknown switch -$sw\n"; }
+		}
+	}
+	else {
+		push( @files, $arg ); }
+}
+
+if($bVerbose) {
+	if($bAuto) {
+		printf STDERR ("Automatically calculated scale $scale and offset %1.3f\n", $offset);
+	}
+	else {
+		print STDERR "Using scale $scale and offset $offset\n";
+	}
+}
+
+@insertInd = sort(@insertInd);
+@inserTime = sort(@inserTime);
+
+my $LE = "\n";
+if($bCRLF) {
+	$LE = "\r\n"; }
+my $nCleaned = 0;
+my @starts = ();
+my @ends = ();
+my @subs = ();
+
+# Parse the file into subs
+foreach my $file (@files) {
+	my $malform = 0;
+
+	# Sniff the encoding of the file
+	my $enc = sniffEncoding($file);
+	($bHasBOM,$encodingIn) = split(',',$enc);
+ 	binmode STDERR, ":encoding($encodingIn)";
+	if($bVerbose) {
+		print STDERR "Encoding for file `$file' detected as `$encodingIn'";
+		if($bHasBOM) { print STDERR ", with BOM\n"; }
+		else { print STDERR "\n"; }
+	}
+	# Set the 'tri-state' to the input state if it is 'high impedance'.
+	if( $tSaveBOM < 0 ) {
+		$tSaveBOM = $bHasBOM; }
+	# TODO: allow choosing any output encoding.
+	if( !defined($encodingOut) ) {
+		$encodingOut = $encodingIn; }
+
+	open( FILE, "<:encoding($encodingIn)", $file ) or die "Can't open file `$file'\n";
+	my $state = 0; # 0 = looking for next time stamp, 1 = inside sub
+	my $idxOld = 0;
+	my $bFirst = 1;
+	
+	foreach my $line (<FILE>) {
+		chomp($line);
+		$line =~ s/\r$//;
+		if($bFirst) {
+			$bFirst = 0;
+			if( $bHasBOM && $line =~ /^\x{feff}/ ) {
+				$line =~ s/^\x{feff}//; } # The BOM is unicode character U+FEFF, regardless of encoding
+		}
+		if( $state == 0 ) {
+			if( ($idxOld) = $line =~ /^\s*(\d+)\s*$/ ) { # Subtitle index
+				while( $#insertInd > -1 && $idxOld >= $insertInd[0] ) { # -i
+					my $tm = 0;
+					if( $#ends > -1 ) { $tm = $ends[$#ends]; }
+					push(@starts, $tm);
+					push(@ends, $tm);
+					push(@subs, "NEW SUB$LE");
+					shift(@insertInd);
+				}
+			}
+			elsif( $line =~ /^\d\d:\d\d:\d\d,\d+ --?> \d\d:\d\d:\d\d,\d+/ ) {
+				$state = 1;
+				my ($tStart,$tEnd) = split(/ --?> /, $line);
+				($tStart,$tEnd) = (fromHMS($tStart),fromHMS($tEnd));
+				while( $#inserTime > -1 && $tStart >= $inserTime[0] ) { # -j
+					my $tNext = $tStart;
+					if( $#inserTime > 0 && $tStart > $inserTime[1] ) {
+						$tNext = $inserTime[1]; }
+					push(@starts, $scale*$inserTime[0]+$offset);
+					push(@ends, $scale*$tNext+$offset);
+					push(@subs, "NEW SUB$LE");
+					shift(@inserTime);
+				}
+
+				push(@starts, $scale*$tStart+$offset);
+				push(@ends, $scale*$tEnd+$offset);
+				push(@subs, '');
+			}
+			elsif( $line ne '' ) {
+				$malform++;
+				if( $malform > 20 ) {
+					print STDERR "Too many unparseable lines, this file probably has bad syntax. Aborting.\n";
+					exit(1);
+				}
+				if($bVerbose) {
+					print STDERR "Ignoring spurious line `$line'\n"; }
+			}
+		}
+		elsif( $state == 1 ) {
+			# FIXME: maybe better to demand a strictly empty line. Using whitespace to
+			# make gaps in a single subtitle could be useful.
+#			if( $line =~ /^\s*$/ ) { # End of the sub
+			if( $line eq '' ) { # End of the sub
+				$state = 0;
+			}
+			else {
+				$subs[$#subs] .= "$line$LE";
+			}
+		}
+	}
+
+	close FILE;
+}
+
+if($bInPlace) {
+	open FILE, ">:encoding($encodingOut)", $files[0] or die "Can't open file `$files[0]' for writing\n";
+	select(FILE);
+}
+
+# Process the subs (if needed) and output
+binmode STDOUT, ":encoding($encodingOut)";
+if( $encodingOut =~ /^UTF-/i && $tSaveBOM ) {
+	printf( '%c', 0xfeff ); }
+my $idxNew = 1;
+
+for( my $s=0; $s<=$#subs; $s++ ) {
+	if($bNukeHA) { # Remove (CLEARS THROAT) and other junk like that
+		$subs[$s] =~ s/\([A-Z0-9 ,.\-'"\n]+?\)//g;
+		$subs[$s] =~ s/\[[A-Z0-9 ,.\-'"\n]+?\]//g;
+		if($bNukeHarder) { # Also remove lowercase and allow more varied formatting
+			$subs[$s] =~ s/-? ?\([A-Z0-9 ,.\-'"\n]+?\)//gi;
+			$subs[$s] =~ s/-? ?\[[A-Z0-9 ,.\-'"\n]+?\]//gi;
+			$subs[$s] =~ s/\n[A-Z0-9 '"#]+?: /- /gi;
+			$subs[$s] =~ s/[A-Z0-9 '"#]+?: *\n//gi;
+			$subs[$s] =~ s/-[A-Z0-9 '"#]+?: /- /gi;
+			$subs[$s] =~ s/[A-Z0-9 '"#]+?: //gi;
+		}
+		else {
+			$subs[$s] =~ s/\n[A-Z0-9 '"#]+?: /\n- /g;
+			$subs[$s] =~ s/[A-Z0-9 '"#]+?:[ \n]//g;
+		}
+		$subs[$s] =~ s/^\n+([^\n])/$1/g; # Remove trailing empty lines
+	}
+	if( $bClean && $subs[$s] =~ /^(<.>\n*<\/.>)?\n*$/ ) { # -c: Skip if empty
+		$nCleaned++;
+		next;
+	}
+	if($bCheckLength) {
+		# First, check for and optionally fix overlap
+		if( $s < $#subs && $starts[$s+1]-$ends[$s] < 0 ) {
+			print STDERR ("Sub $idxNew overlaps with next");
+			if($bFixLength) {
+				$ends[$s] = $starts[$s+1]-$gap;
+				print STDERR " -> Fixed";
+			}
+			print STDERR "\n";
+		}
+
+		# Check the duration of a sub vs. the length of its 'canonical form'
+		my $sub = ']'.$subs[$s];
+		$sub =~ s/\s\s+/ /g;
+		$sub =~ s/\s?$LE/]/g;
+		$sub =~ s/\]\s/]/g;
+		$sub =~ s/\.\.\././g;
+		my $dur = $ends[$s]-$starts[$s];
+		my $okDur = $minRatio*length($sub);
+		if( $okDur < $minDur ) { $okDur = $minDur; }
+		if( $dur < $okDur ) {
+			printf STDERR ("Sub $idxNew too fast: %.2f < %.2f (at %s)",
+			               $dur, $okDur, toHMS($starts[$s]) );
+			if($bFixLength) {
+				my $newEnd = $starts[$s]+1.05*$okDur;
+				if( $s == $#subs || int(.5+1000*$starts[$s+1]) >= int(.5+1000*($newEnd+$gap)) ) {
+					$ends[$s] = $newEnd;
+					print STDERR " -> Fixed";
+				}
+				elsif( int(.5+1000*($starts[$s+1]-$gap)) > int(.5+1000*$ends[$s]) ) {
+					$ends[$s] = $starts[$s+1]-$gap;
+					printf STDERR (" -> Partially fixed (%.2f)", ($starts[$s+1]-$gap-$starts[$s]) );
+				}
+				else {
+					print STDERR " -> Cannot fix"; }
+			}
+			print STDERR "\n";
+		}
+		elsif( $dur > 3 && $dur > $stickRatio*length($sub) ) {
+			printf STDERR ("Sub $idxNew seems sticky: %.2f secs (at %s)\n",
+			               $dur, toHMS($starts[$s]) ); }
+	}
+	if($bTextOnly) {
+		print $subs[$s] . $LE;
+	}
+	else {
+		print "$idxNew$LE".
+		      toHMS($starts[$s]) .' --> '. toHMS($ends[$s]) . $LE .
+		      $subs[$s] . $LE;
+	}
+	$idxNew++;
+}
+
+if( $bClean && $bVerbose) {
+	print STDERR "Removed $nCleaned empty subtitles.\n";
+}
+
+
+##############################################################
+# SUBROUTINES
+
+sub isFloat
+{
+	return ( $_[0] =~ /^-?\d*\.?\d+$/ );
+}
+
+sub isHMS
+{
+	return ( $_[0] =~ /^-?\d\d:\d\d:\d\d([,\.]\d+)?$/ );
+}
+
+# Convert HH:MM:SS[.,]mmm to floating-point number
+sub fromHMS
+{
+	my ($hms) = @_;
+	if( isFloat($hms) ) { return $hms; }
+	my $neg = 1;
+	if( $hms =~ /^-/ ) {
+		$neg = -1;
+		$hms = substr($hms,1);
+	}
+	my ($h,$m,$s) = (substr($hms,0,2), substr($hms,3,2), substr($hms,6,2));
+	if( length($hms) > 9 ) {
+		$hms =~ s/,/./;
+		$s += substr($hms,8);
+	}
+	return $neg*($s+60*($m+60*$h));
+}
+
+# Convert floating-point number to HH:MM:SS,mmm
+sub toHMS
+{
+	my $neg = '';
+	my ($ip,$fp) = split(/\./, $_[0]);
+	if($ip < 0) {
+		$neg = '-';
+		$ip *= -1;
+	}
+	if( ! defined($fp) ) { $fp = 0; }
+	my $s = ($ip % 60) . ".$fp";	
+	my $m = int($ip/60)%60;
+	my $h = int($ip/3600);
+	my $hms = sprintf("%02d:%02d:%06.3f", $h,$m,$s);
+	$hms =~ s/\./,/;
+	return "$neg$hms";
+}
+
+sub isScale
+{
+	return ((isFloat($_[0]) && $_[0] >= 0) || $_[0] =~ /^(NTSC(PAL|FILM)|PAL(NTSC|FILM)|FILM(NTSC|PAL))$/i);
+}
+
+sub fromScale
+{
+	if( isFloat($_[0]) ) { return $_[0]; }
+	my ($from,$to) = (1,1);
+	if   ( $_[0] =~ /^NTSC/i ) { $from = 23.976; }
+	elsif( $_[0] =~ /^PAL/i )  { $from = 25; }
+	elsif( $_[0] =~ /^FILM/i ) { $from = 24; }
+	if   ( $_[0] =~ /NTSC$/i ) { $to = 23.976; }
+	elsif( $_[0] =~ /PAL$/i )  { $to = 25; }
+	elsif( $_[0] =~ /FILM$/i ) { $to = 24; }
+	return $from/$to;
+}
+
+sub sniffEncoding
+# 'Sniff' the encoding of a file, and the presence of a BOM character.
+# Return value is "$bHasBOM,$encoding".
+# Ideally, this function would be able to identify any encoding on any file,
+#   even without the presence of any BOM. For 8-bit encodings this could be
+#   done by looking at statistics of the non-ASCII codes, for UTF-n it can be
+#   done by looking if the stream is valid UTF-8, UTF-16LE etc.
+# But for now this primitive unfinished version will only recognize UTF-8 and
+#   UTF-16 files with a BOM, and otherwise assume 'Windows Latin 1' (cp1252).
+# TODO: improve this!
+{
+	open FILE, '<:bytes', $_[0] or die "Can't open file `$_[0]'\n";
+	my $line = <FILE>;
+	close FILE;
+	# Beware that this is different if perl treats the string as utf-8/16, in
+	# that case the BOM is represented by by \x{feff}
+	if( $line =~ /^\x{ef}\x{bb}\x{bf}/ ) {
+		return '1,UTF-8'; }
+	elsif( $line =~ /^\x{fe}\x{ff}/ ) {
+		return '1,UTF-16BE'; }
+	elsif( $line =~ /^\x{ff}\x{fe}/ ) {
+		return '1,UTF-16LE'; }
+	else {
+		return '0,cp1252'; }
+#		return '0,iso-8859-5'; }
+}
+
+# note: to get list of supported encodings:
+# perl -MEncode -le "print for Encode->encodings(':all')"
