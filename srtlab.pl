6bd0e1c5d9a8361cebaca9183d9f04f25a0620ef
Alexander Thomas <doctor.lex@gmail.com>
2017-09-24 20:50:05 +0200

    Release 0.98: add -f option for OCR fix.

--- srtlab.pl
+++ srtlab.pl
@@ -11,6 +11,7 @@
 # Version 0.96 (2012/07): overlap detection and removal
 # Version 0.97 (2017/01): URL removal, more robust against poor formatting, much
 #   better encoding detection; [Idiomdrottning] whitespace removal, -HH tweaks.
+# Version 0.98 (2012/09): rudimentary OCR error fix option
 #
 # Copyright (C) 2017  Alexander Thomas & Idiomdrottning
 #
@@ -33,7 +34,7 @@ use utf8;
 use Encode qw(decode encode);
 use Encode::Guess;
 
-my $VERSION = '0.97';
+my $VERSION = '0.98';
 
 # TODO: allow the user to override input encoding detection, or to configure it.
 # TODO: further improve -HH to remove more variants without breaking regular dialogue.
